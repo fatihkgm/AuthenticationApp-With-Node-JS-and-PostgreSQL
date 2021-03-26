4a4af35696335acf6e9f9fb30c8d31e108d7d59b
fatih kgokmen <fatih.kgokmen@gmail.com>
2021-03-26 16:24:33 -0400

    config file created for darabase

--- app.js
+++ app.js
@@ -1,30 +1,43 @@
 const express = require('express');
 const app = express();
+const { pool } = require('./config');
 const ejs = require('ejs');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+app.use(express.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.get('/register', (req, res) => {
-  res.render('register');
+app.get('/users/register', (req, res) => {
+  res.render('register.ejs');
 });
-app.get('/login', (req, res) => {
-  res.render('login');
+app.get('/users/login', (req, res) => {
+  res.render('login.ejs');
 });
 
-app.get('/userpanel', (req, res) => {
-  res.render('userpanel');
+app.get('/users/userpanel', (req, res) => {
+  res.render('userpanel.ejs');
+});
+
+app.post('/users/register', (req, res) => {
+  let { name, email, password, password_confirm } = req.body;
+  console.log({
+    name,
+    email,
+    password,
+    password_confirm,
+  });
+  let errors = [];
 });
 
 let port = process.env.PORT;
 if (port == null || port == '') {
   port = 5000;
 }
-
 app.listen(port, function () {
   console.log(`Server has started successfully at ${port}`);
 });
