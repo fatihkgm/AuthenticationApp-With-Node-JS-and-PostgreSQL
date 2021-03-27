20a755ec000e03a55503bf0590830d78b5dbcfe8
fatih kgokmen <fatih.kgokmen@gmail.com>
2021-03-27 02:01:34 -0400

    passport authentication implemented

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ const ejs = require('ejs');
 const bcrypt = require('bcrypt');
 const session = require('express-session');
 const flash = require('express-flash');
+const passport = require('passport');
+require('dotenv').config();
+const initializePassport = require('./pswConfig');
+initializePassport(passport);
 
-app.set('view engine', 'ejs');
 app.use(express.static('public'));
-app.use(flash());
 
 app.use(
   session({
@@ -17,23 +19,31 @@ app.use(
     saveUninitialized: false,
   })
 );
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 
-app.use;
 app.use(express.urlencoded({ extended: false }));
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.get('/users/register', (req, res) => {
-  res.render('register.ejs');
+app.get('/users/register', checkAuthenticated, (req, res) => {
+  res.render('register');
 });
-app.get('/users/login', (req, res) => {
-  res.render('login.ejs');
+app.get('/users/login', checkAuthenticated, (req, res) => {
+  res.render('login');
 });
 
-app.get('/users/userpanel', (req, res) => {
-  res.render('userpanel.ejs');
+app.get('/users/userpanel', checkNotAuthenticated, (req, res) => {
+  res.render('userpanel', { user: req.user.name });
+});
+
+app.get('/users/logout', (req, res) => {
+  req.logout();
+  res.render('index', { message: 'You have logged out successfully' });
 });
 
 app.post('/users/register', async (req, res) => {
@@ -49,7 +59,7 @@ app.post('/users/register', async (req, res) => {
   if (!name || !email || !password || !password_confirm) {
     errors.push({ message: 'Please enter all field correctly' });
   }
-  if (password.length < 8) {
+  if (password.length < 6) {
     errors.push({ message: 'Password must be 6 characters long' });
   }
 
@@ -60,7 +70,7 @@ app.post('/users/register', async (req, res) => {
   if (errors.length > 0) {
     res.render('register', { errors, name, email, password, password_confirm });
   } else {
-    let hashedPassword = await bcrypt.hash(password, 10);
+    hashedPassword = await bcrypt.hash(password, 10);
     console.log(hashedPassword);
     pool.query(
       `SELECT * FROM users
@@ -68,16 +78,17 @@ app.post('/users/register', async (req, res) => {
       [email],
       (err, results) => {
         if (err) {
-          throw err;
+          console.log(err);
         }
         console.log(results.rows);
         if (results.rows.length > 0) {
-          errors.push({ message: 'You have already registered' });
-          res.render('register', { errors });
+          return res.render('register', {
+            message: 'Email already registered',
+          });
         } else {
           pool.query(
             `INSERT INTO users (name,email,password)
-            VALUES ($1, $2,$3)
+            VALUES ($1, $2, $3)
             RETURNING id,password `,
             [name, email, hashedPassword],
             (err, results) => {
@@ -94,6 +105,29 @@ app.post('/users/register', async (req, res) => {
     );
   }
 });
+
+app.post(
+  '/users/login',
+  passport.authenticate('local', {
+    successRedirect: '/users/userpanel',
+    failureRedirect: '/users/login',
+    failureFlash: true,
+  })
+);
+
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect('/users/dashboard');
+  }
+  next();
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/users/login');
+}
 
 let port = process.env.PORT;
 if (port == null || port == '') {
